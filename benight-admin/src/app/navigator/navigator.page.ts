8c249594f6b21fc9155913455e956c4f72d6d153
idanzap <samir_abdallah@hotmail.es>
2018-10-23 18:13:56 +0200

    minor changes

--- benight-admin/src/app/navigator/navigator.page.ts
+++ benight-admin/src/app/navigator/navigator.page.ts
@@ -17,7 +17,7 @@ export class NavigatorPage implements OnInit {
   generalTabs = tabs.generalTabs
   
   database = database
-  title:string = "Eventos"
+  header:string = "Eventos"
   back:boolean = false
 
   empty:boolean = true
@@ -33,28 +33,11 @@ export class NavigatorPage implements OnInit {
     this.router.events.pipe( 
       filter(event => event instanceof ActivationStart) )
       .subscribe(event => { 
-        console.log(event['snapshot'].data['tabs'])
         this.back = event['snapshot'].data['back']
-        this.title = event['snapshot'].data['header']
+        this.header = event['snapshot'].data['header']
         this.tabs = event['snapshot'].data['tabs']
         this.empty = this.tabs === this.gestion || this.tabs === this.general        
       })
-  }
-  
-  generalTabActive() {    
-    return this.tabs === this.general
-  }  
-  
-  gestionTabActive() {
-    return this.tabs === this.gestion
-  }
-
-  isBackActive(){
-    return this.back
-  }
-  
-  header(){
-    return this.title  
   }
   
   backUrl(){
